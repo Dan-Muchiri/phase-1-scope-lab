80231eba0ef5dd680218fa74dd3fad7f6d9bfd1c
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-13 14:44:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+return customerName;
+}
+
+function setBestCustomer(){
+     bestCustomer = "not bob";
+return bestCustomer;
+}
+
+function overwriteBestCustomer(best){
+    bestCustomer = best;
+    return bestCustomer;
+}
+const leastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer(least){
+    leastFavoriteCustomer =least;
+    return leastFavoriteCustomer;
+}
